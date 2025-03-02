c25e250061babdb722694e0f0ae7e8b6af246c6d
jfuregatti <jfuregatti@gmail.com>
2025-03-02 13:27:45 -0300

    add the setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+set -x;
+xargs -a ./packages.txt sudo apt install;
+
+cat ./.vimrc >> ~/.vimrc;
+cat ./.tmux.conf >> ~/.tmux.conf;
+cat ./.bash_aliases >> ~/.bash_aliases;
+
+set +x;
 
